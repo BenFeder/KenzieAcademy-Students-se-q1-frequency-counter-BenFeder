d55dc5daa4b52d1fcbc0c76d4f115c2245e52a5d
kenzieadmin <kenzieadmin@users.noreply.github.com>
2020-09-10 17:58:14 +0000

    deploy: c8d433fa43d2605afe462c927bc88bda1bc7b4f3

--- code.js
+++ code.js
@@ -1,65 +1,63 @@
 // Your Code here
-
-function countLettersAndWords() {
-  // your code will go here ...
-
+function countWords() {
   let typedText = document.getElementById("textInput").value;
   typedText = typedText.toLowerCase();
+  typedText = typedText.replace(/[^a-z'\s]+/g, "");
 
-  function countWords(text) {
-    text = text.replace(/[^a-z'\s]+/g, "");
-    let words = text.split(" ");
-    let wordCounts = {};
+  let words = typedText.split(" ");
+  let wordCounts = {};
 
-    for (let i = 0; i < words.length; i++) {
-      let currentWord = words[i];
-      // do something for each word.
-      if (wordCounts[currentWord] === undefined) {
-        wordCounts[currentWord] = 1;
-      } else {
-        wordCounts[currentWord]++;
-      }
-    }
-
-    for (let word in wordCounts) {
-      const spanWords = document.createElement("span");
-      const textContentWords = document.createTextNode(
-        '"' + word + '": ' + wordCounts[word] + ", "
-      );
-      spanWords.append(textContentWords);
-      document.getElementById("wordsDiv").append(spanWords);
+  for (let i = 0; i < words.length; i++) {
+    let currentWord = words[i];
+    // do something for each word.
+    if (wordCounts[currentWord] === undefined) {
+      wordCounts[currentWord] = 1;
+    } else {
+      wordCounts[currentWord]++;
     }
   }
 
-  function countLetters(text) {
-    let letterCounts = {};
+  for (let word in wordCounts) {
+    const spanWords = document.createElement("span");
+    const textContentWords = document.createTextNode(
+      '"' + word + '": ' + wordCounts[word] + ", "
+    );
+    spanWords.append(textContentWords);
+    document.getElementById("wordsDiv").append(spanWords);
+  }
 
-    for (let i = 0; i < typedText.length; i++) {
-      let currentLetter = typedText[i];
-      // do something for each letter.
-      if (letterCounts[currentLetter] === undefined) {
-        letterCounts[currentLetter] = 1;
-      } else {
-        letterCounts[currentLetter]++;
-      }
-    }
+  event.preventDefault();
+}
 
-    for (let letter in letterCounts) {
-      const span = document.createElement("span");
-      const textContent = document.createTextNode(
-        '"' + letter + '": ' + letterCounts[letter] + ", "
-      );
-      span.append(textContent);
-      document.getElementById("lettersDiv").append(span);
+function countLetters() {
+  let typedText = document.getElementById("textInput").value;
+  typedText = typedText.toLowerCase();
+  typedText = typedText.replace(/[^a-z'\s]+/g, "");
+
+  let letterCounts = {};
+
+  for (let i = 0; i < typedText.length; i++) {
+    let currentLetter = typedText[i];
+    // do something for each letter.
+    if (letterCounts[currentLetter] === undefined) {
+      letterCounts[currentLetter] = 1;
+    } else {
+      letterCounts[currentLetter]++;
     }
   }
 
-  //run functions
-  countWords(typedText);
-  countLetters(typedText);
+  for (let letter in letterCounts) {
+    const span = document.createElement("span");
+    const textContent = document.createTextNode(
+      '"' + letter + '": ' + letterCounts[letter] + ", "
+    );
+    span.append(textContent);
+    document.getElementById("lettersDiv").append(span);
+  }
 
   event.preventDefault();
 }
 
 countLettersAndWordsButton = document.getElementById("countButton");
-countLettersAndWordsButton.addEventListener = ("click", countLettersAndWords);
+countLettersAndWordsButton.addEventListener("click", countWords);
+countLettersAndWordsButton.addEventListener("click", countLetters);
